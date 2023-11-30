3373430e54337149caba93367be2f84d165ac7c0
Elias Saarenala <elias.saarenala@elev.ga.ntig.se>
2023-11-30 11:01:35 +0100

    v1

--- vinter projekt 1/Program.cs
+++ vinter projekt 1/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+//int[,] map = new int[3, 4]; 
+//map[1] = 1;
+//for(int y = 0; y < map.GetLength(1); y++){
+//    for(int x = 0; x< map.GetLength(0); x++){
+//        Console.WriteLine(map[x,0]);
+//        Count++;
+//    }
+//}
+//int [] numbers = {1, 2, 3, 4, 5};
+//string name = "micke";
+//numbers[0] = 25;
+
+//char[] letters = {'e'};
+//Console.WriteLine(numbers[]);
+//Console.ReadLine();
+//(int hp,string name) character =(100,"elias");
+//static () saygodbye(int inrehp){}
+
+
+
+
+//string[]playerName= {};
+
+
+
+
+Console.WriteLine("hello and welcome to my DnD cheet creator \nName your charachter \nPress Enter");
+string characterName = Console.ReadLine().ToLower();
+while ( string.IsNullOrEmpty (characterName) ||string.IsNullOrWhiteSpace (characterName)){
+    Console.WriteLine("skriv ett namn");
+    characterName = Console.ReadLine();
+}
+
+
+
+
+
+
+Console.ReadLine();
+
